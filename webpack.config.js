30c4521245f3de25f3b14c7e7cf9702146e78d26
Jason Walsh <jwalsh@saylent.com>
2017-07-05 11:14:15 -0400

    Webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
-var webpack = require("webpack");
+var webpack = require('webpack');
 var PACKAGE = require('./package.json');
+var path = require('path');
 
 var banner = PACKAGE.name + ' - ' + PACKAGE.version;
 
 module.exports = {
 
-  entry: "./dist/index.js",
-  devtool: "source-map",
+  entry: './dist/index.js',
+  devtool: 'source-map',
   output: {
-    path: "./dist",
-    filename: "bundle.min.js"
+    path: path.join(__dirname, './dist'),
+    filename: 'bundle.min.js'
   },
   plugins: [
     new webpack.optimize.UglifyJsPlugin({minimize: true}),
